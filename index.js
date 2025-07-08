a1196fcfd78d7fcc0756db2ac1e206d7d0632aee
LouisYangga <102441218+LouisYangga@users.noreply.github.com>
2025-07-08 12:44:39 +1000

    CORS policy

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import http from 'http';
 import routes from './routes/onboardingRoutes.js';
+import cors from 'cors';
 import { Server } from 'socket.io';
 dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {cors: { origin: '*' } });
+const allowedOrigins = [
+  'http://localhost:5173',
+  "https://portfolio-with-chatbot-five.vercel.app",  // Vercel-provided domain
+  "https://louisyangga.com", 
+  "https://www.louisyangga.com"];
+
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization','x-api-key']
+}));
 
 app.use(express.json());
 app.use('/api', routes);
